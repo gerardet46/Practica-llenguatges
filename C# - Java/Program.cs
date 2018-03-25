573d1ca7399894a79e6eaa18954123f58d77ccb9
GerrySoft <33908768+GerrySoft@users.noreply.github.com>
2018-03-25 16:30:07 +0200

    Punt d'entrada de l'aplicaió
    Estableix les configuracions per córrer el formulari que toca correctament

--- C# - Java/Program.cs
+++ C# - Java/Program.cs
@@ -9,14 +9,14 @@ namespace WinForms_CS
     static class Program
     {
         /// <summary>
-        /// Punto de entrada principal para la aplicación.
+        /// Punt d'entrada principal de l'aplicaió
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmpol());
+            Application.Run(new frmpol()); // Executar el formulari corresponent
         }
     }
 }
