0af3f4ef4a11dda76a311bf5074ec27a78a566fb
GerrySoft <33908768+GerrySoft@users.noreply.github.com>
2018-03-26 19:20:56 +0200

    Arxiu executable pel Windows
    No obrir en Mac, per obrir-ho en Windows, NET. Framework 4.5+

---
+++ C# - Java/Rellotge analògic (C# - Gràfics primitius)/frm rellotge analògic.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WinForms_CS
+{
+    partial class frmrellotge_analògic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbhora = new System.Windows.Forms.Label();
+            this.separador1 = new WinForms_CS.separador();
+            this.SuspendLayout();
+            // 
+            // lbhora
+            // 
+            this.lbhora.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbhora.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.lbhora.ForeColor = System.Drawing.Color.White;
+            this.lbhora.Location = new System.Drawing.Point(0, 0);
+            this.lbhora.Name = "lbhora";
+            this.lbhora.Size = new System.Drawing.Size(732, 127);
+            this.lbhora.TabIndex = 0;
+            this.lbhora.Text = "Hora";
+            this.lbhora.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // separador1
+            // 
+            this.separador1.BackColor = System.Drawing.Color.White;
+            this.separador1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.separador1.Grosor = 3;
+            this.separador1.Location = new System.Drawing.Point(0, 127);
+            this.separador1.Name = "separador1";
+            this.separador1.Size = new System.Drawing.Size(732, 3);
+            this.separador1.TabIndex = 1;
+            this.separador1.Tipus = WinForms_CS.separador.TIPUS.HORITZONTAL;
+            // 
+            // frmrellotge_analògic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(732, 658);
+            this.Controls.Add(this.separador1);
+            this.Controls.Add(this.lbhora);
+            this.Name = "frmrellotge_analògic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "rellotge analògic";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbhora;
+        private separador separador1;
+    }
+}
---
+++ C# - Java/Rellotge analògic (C# - Gràfics primitius)/frm rellotge analògic.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinForms_CS
+{
+    public partial class frmrellotge_analògic : Form
+    {
+        float sin(float deg) // Tenint en compte la y
+        {
+            bool neg = true;
+
+            if (deg == 0 || deg == 180) return 0;
+            if (deg == 90) return -1;
+            if (deg == 270) return 1;
+
+            if (deg > 180) neg = false;
+            if (deg >= 180) deg -= 180;
+
+            float r = (float)Math.Sin(deg * Math.PI / 180);
+            return neg ? -r : r;
+        }
+        float cos(float deg) // Tenint en compte la x
+        {
+            bool neg = true;
+
+            if (deg == 0) return 1;
+            if (deg == 90 || deg == 270) return 0;
+            if (deg == 180) return -1;
+
+            if (deg < 180) neg = false;
+            if (deg > 180) deg -= 180;
+
+            float r = (float)Math.Cos(deg * Math.PI / 180);
+            return neg ? -r : r;
+        }
+        float simplificar(float deg, int interval = 360)
+        {
+            if (deg < 0) return simplificar(360 + deg);
+            if (deg >= interval) return simplificar(deg - interval);
+            return deg;
+        }
+
+        Color fff = Color.White;
+        public frmrellotge_analògic()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(100, 100);
+            SizeChanged += (s, e) => Refresh();
+            Paint += paint;
+            while (DateTime.Now.Millisecond > 450 && DateTime.Now.Millisecond < 550) { }
+            Timer t = new Timer() { Interval = 1000, Enabled = true };
+            t.Tick += (s, e) => pintar_hora();
+        }
+
+        float cx, cy, r, ant1, ant2, ant3;
+        void pintar_hora()
+        {
+            DateTime n = DateTime.Now;
+            int h = n.Hour;
+            if (h > 12) h -= 12;
+            int s = h * 3600 + n.Minute * 60 + n.Second;
+            lbhora.Text = $"{n.Hour.ToString("d2")}:{n.Minute.ToString("d2")}:{n.Second.ToString("d2")}";
+
+            Graphics e = this.CreateGraphics();
+
+            float ah = simplificar(90 - s / 120), am = simplificar(90 - s / 10), aseg = simplificar(90 - n.Second * 6);
+
+            // Despintar línees antigues
+            e.DrawLine(new Pen(Color.SeaGreen, 8), cx, cy, cx + (r * 0.9f) * cos(ant2), cy + (r * 0.9f) * sin(ant2)); // minuts
+            e.DrawLine(new Pen(Color.SeaGreen, 15), cx, cy, cx + (r * 0.4f) * cos(ant1), cy + (r * 0.4f) * sin(ant1)); // hores
+            e.DrawLine(new Pen(Color.SeaGreen, 1), cx, cy, cx + (r * 0.95f) * cos(ant3), cy + (r * 0.95f) * sin(ant3)); // segons
+
+            // Segments de referècia
+            for (int i = 0; i < 60; i++)
+            {
+                float an = i * 6;
+                int thick = 2, llarg = 20;
+                if (i % 5 == 0)
+                {
+                    thick = 8;
+                    llarg = 35;
+                }
+                e.DrawLine(new Pen(fff, thick), cx + (r - 5) * cos(an), cy + (r - 5) * sin(an), cx + (r - llarg) * cos(an), cy + (r - llarg) * sin(an));
+            }
+
+            // Pintar noves
+            e.DrawLine(new Pen(fff, 8), cx, cy, cx + (r * 0.9f) * cos(am), cy + (r * 0.9f) * sin(am)); // minuts
+            e.DrawLine(new Pen(fff, 15), cx, cy, cx + (r * 0.4f) * cos(ah), cy + (r * 0.4f) * sin(ah)); // hores
+            e.DrawLine(new Pen(Color.DarkRed, 1), cx, cy, cx + (r * 0.95f) * cos(aseg), cy + (r * 0.95f) * sin(aseg)); // segons
+
+            ant1 = ah;
+            ant2 = am;
+            ant3 = aseg;
+        }
+
+        private void paint(object sender, PaintEventArgs e)
+        {
+            // Només cercle principal
+            cx = Width / 2;
+            cy = (Height + lbhora.Height - 40) / 2;
+            r = cy - lbhora.Height - 25;
+
+            pintar_hora();
+
+            e.Graphics.DrawEllipse(new Pen(fff, 5), cx - r, cy - r, 2 * r, 2 * r);
+            e.Graphics.FillEllipse(Brushes.SeaGreen, cx - r, cy - r, 2 * r, 2 * r);
+        }
+    }
+}
