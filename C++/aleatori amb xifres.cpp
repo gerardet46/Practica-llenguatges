a2533794795ed9f85a0677fe7fd17ed9e470ab1f
GerrySoft <33908768+GerrySoft@users.noreply.github.com>
2018-03-26 19:23:33 +0200

    Crear numero aleatori amb x xifres
    Aquest script permet generar un número positiu sencer aleatori amb es numero de xifres que diguis (la primera no és 0)

--- C++/aleatori amb xifres.cpp
+++ C++/aleatori amb xifres.cpp
@@ -3,34 +3,8 @@
 #include <conio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <windows.h>
 
 using namespace std;
-
-void clipboard(string a) {
-	/*const char* b = a.c_str();
-	const size_t len = strlen(b) + 1;
-    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
-    memcpy(GlobalLock(hMem), b, len);
-    GlobalUnlock(hMem);
-    OpenClipboard(0);
-    EmptyClipboard();
-    SetClipboardData(CF_TEXT, hMem);
-    CloseClipboard();*/
-    HWND hwnd = ::GetActiveWindow();
-    OpenClipboard(hwnd);
-	EmptyClipboard();
-	HGLOBAL hg=GlobalAlloc(GMEM_MOVEABLE,a.length()+1);
-	if (!hg){
-		CloseClipboard();
-		return;
-	}
-	memcpy(GlobalLock(hg),a.c_str(),a.length()+1);
-	GlobalUnlock(hg);
-	SetClipboardData(CF_TEXT,hg);
-	CloseClipboard();
-	GlobalFree(hg);
-}
 
 int main(int argc, char const *argv[])
 {
@@ -53,7 +27,7 @@ int main(int argc, char const *argv[])
 				cout << n;
 				r += n;
 			}
-			cout << "\nNumero copiat";
+			cout << "\nNombre copiat";
 			getch();
 		} else if (r == '2') break;
 	}
