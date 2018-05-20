92e8fe728e5e0b1b9b914900779554f9cfe4ddf3
GerrySoft <33908768+GerrySoft@users.noreply.github.com>
2018-05-20 12:52:52 +0200

    Canvi de nom de carpeta

---
+++ C#/El joc de la vida de Conway/joc de la vida de conway.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WinForms_CS
+{
+    partial class conway
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(conway));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.amplada = new System.Windows.Forms.TrackBar();
+            this.velocitat = new System.Windows.Forms.TrackBar();
+            this.btnborrar = new System.Windows.Forms.Button();
+            this.btnproxim = new System.Windows.Forms.Button();
+            this.btnplay = new System.Windows.Forms.Button();
+            this.lbample = new System.Windows.Forms.Label();
+            this.lbvel = new System.Windows.Forms.Label();
+            this.lbgen = new System.Windows.Forms.Label();
+            this.btnobrir = new System.Windows.Forms.Button();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.cbgraella = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.amplada)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.velocitat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(14, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(790, 852);
+            this.panel1.TabIndex = 0;
+            // 
+            // amplada
+            // 
+            this.amplada.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.amplada.Location = new System.Drawing.Point(814, 177);
+            this.amplada.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.amplada.Maximum = 70;
+            this.amplada.Minimum = 10;
+            this.amplada.Name = "amplada";
+            this.amplada.Size = new System.Drawing.Size(344, 56);
+            this.amplada.TabIndex = 1;
+            this.amplada.Value = 25;
+            // 
+            // velocitat
+            // 
+            this.velocitat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.velocitat.LargeChange = 2;
+            this.velocitat.Location = new System.Drawing.Point(818, 289);
+            this.velocitat.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.velocitat.Maximum = 25;
+            this.velocitat.Minimum = 1;
+            this.velocitat.Name = "velocitat";
+            this.velocitat.Size = new System.Drawing.Size(340, 56);
+            this.velocitat.TabIndex = 2;
+            this.velocitat.Value = 2;
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnborrar.Location = new System.Drawing.Point(814, 427);
+            this.btnborrar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(344, 68);
+            this.btnborrar.TabIndex = 3;
+            this.btnborrar.Text = "&Borrar (B)";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            // 
+            // btnproxim
+            // 
+            this.btnproxim.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnproxim.Location = new System.Drawing.Point(814, 507);
+            this.btnproxim.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnproxim.Name = "btnproxim";
+            this.btnproxim.Size = new System.Drawing.Size(344, 68);
+            this.btnproxim.TabIndex = 4;
+            this.btnproxim.Text = "&Pròxima generació (P)";
+            this.btnproxim.UseVisualStyleBackColor = true;
+            // 
+            // btnplay
+            // 
+            this.btnplay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnplay.Location = new System.Drawing.Point(814, 587);
+            this.btnplay.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnplay.Name = "btnplay";
+            this.btnplay.Size = new System.Drawing.Size(344, 68);
+            this.btnplay.TabIndex = 5;
+            this.btnplay.Text = "&Iniciar (i)";
+            this.btnplay.UseVisualStyleBackColor = true;
+            // 
+            // lbample
+            // 
+            this.lbample.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbample.AutoSize = true;
+            this.lbample.Location = new System.Drawing.Point(812, 127);
+            this.lbample.Name = "lbample";
+            this.lbample.Size = new System.Drawing.Size(284, 32);
+            this.lbample.TabIndex = 6;
+            this.lbample.Text = "Amplada de cel·la = 25px";
+            // 
+            // lbvel
+            // 
+            this.lbvel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbvel.AutoSize = true;
+            this.lbvel.Location = new System.Drawing.Point(812, 239);
+            this.lbvel.Name = "lbvel";
+            this.lbvel.Size = new System.Drawing.Size(216, 32);
+            this.lbvel.TabIndex = 7;
+            this.lbvel.Text = "Velocitat = 2 gen/s";
+            // 
+            // lbgen
+            // 
+            this.lbgen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbgen.AutoSize = true;
+            this.lbgen.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.lbgen.Location = new System.Drawing.Point(849, 34);
+            this.lbgen.Name = "lbgen";
+            this.lbgen.Size = new System.Drawing.Size(247, 54);
+            this.lbgen.TabIndex = 8;
+            this.lbgen.Text = "1º generació";
+            // 
+            // btnobrir
+            // 
+            this.btnobrir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnobrir.Location = new System.Drawing.Point(814, 816);
+            this.btnobrir.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnobrir.Name = "btnobrir";
+            this.btnobrir.Size = new System.Drawing.Size(344, 68);
+            this.btnobrir.TabIndex = 10;
+            this.btnobrir.Text = "&Obrir distribució (O)";
+            this.btnobrir.UseVisualStyleBackColor = true;
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnguardar.Location = new System.Drawing.Point(814, 736);
+            this.btnguardar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(344, 68);
+            this.btnguardar.TabIndex = 9;
+            this.btnguardar.Text = "&Guardar distribució (G)";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            // 
+            // cbgraella
+            // 
+            this.cbgraella.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbgraella.AutoSize = true;
+            this.cbgraella.Checked = true;
+            this.cbgraella.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbgraella.Location = new System.Drawing.Point(818, 366);
+            this.cbgraella.Name = "cbgraella";
+            this.cbgraella.Size = new System.Drawing.Size(110, 36);
+            this.cbgraella.TabIndex = 11;
+            this.cbgraella.Text = "Graella";
+            this.cbgraella.UseVisualStyleBackColor = true;
+            // 
+            // conway
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 876);
+            this.Controls.Add(this.cbgraella);
+            this.Controls.Add(this.btnobrir);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.lbgen);
+            this.Controls.Add(this.lbvel);
+            this.Controls.Add(this.lbample);
+            this.Controls.Add(this.btnplay);
+            this.Controls.Add(this.btnproxim);
+            this.Controls.Add(this.btnborrar);
+            this.Controls.Add(this.velocitat);
+            this.Controls.Add(this.amplada);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MinimumSize = new System.Drawing.Size(1190, 864);
+            this.Name = "conway";
+            this.Text = "Joc de la vida de Conway";
+            ((System.ComponentModel.ISupportInitialize)(this.amplada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.velocitat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar amplada;
+        private System.Windows.Forms.TrackBar velocitat;
+        private System.Windows.Forms.Button btnborrar;
+        private System.Windows.Forms.Button btnproxim;
+        private System.Windows.Forms.Button btnplay;
+        private System.Windows.Forms.Label lbample;
+        private System.Windows.Forms.Label lbvel;
+        private System.Windows.Forms.Label lbgen;
+        private System.Windows.Forms.Button btnobrir;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.CheckBox cbgraella;
+    }
+}
---
+++ C#/Rellotge analògic (C# - Gràfics primitius)/frm rellotge analògic.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinForms_CS
+{
+    public partial class frmrellotge_analogic : Form
+    {
+        float sin(float deg) // Tenint en compte la y
+        {
+            bool neg = true;
+
+            if (deg == 0 || deg == 180) return 0;
+            if (deg == 90) return -1;
+            if (deg == 270) return 1;
+
+            if (deg > 180) neg = false;
+            if (deg >= 180) deg -= 180;
+
+            float r = (float)Math.Sin(deg * Math.PI / 180);
+            return neg ? -r : r;
+        }
+        float cos(float deg) // Tenint en compte la x
+        {
+            bool neg = true;
+
+            if (deg == 0) return 1;
+            if (deg == 90 || deg == 270) return 0;
+            if (deg == 180) return -1;
+
+            if (deg < 180) neg = false;
+            if (deg > 180) deg -= 180;
+
+            float r = (float)Math.Cos(deg * Math.PI / 180);
+            return neg ? -r : r;
+        }
+        float simplificar(float deg, int interval = 360)
+        {
+            if (deg < 0) return simplificar(360 + deg);
+            if (deg >= interval) return simplificar(deg - interval);
+            return deg;
+        }
+
+        Color fff = Color.White; // Definim el color de línees i busques (la secundària serà vermella)
+        public frmrellotge_analogic()
+        {
+            InitializeComponent();
+            this.MinimumSize = new Size(100, 100); // Tamany mínim del formulari: 100 x 100px
+            SizeChanged += (s, e) => Refresh(); // Si canviam el tamany de la finestra, ajustar el rellotge
+            Paint += paint; // Event per pintar el formulari (this.Paint)
+            while (DateTime.Now.Millisecond > 450 && DateTime.Now.Millisecond < 550) { /*Esperar fins el segon i mig (evitar problemes)*/ }
+            Timer t = new Timer() { Interval = 1000, Enabled = true };
+            t.Tick += (s, e) => pintar_hora(); // cada 1 segon (1000ms), pintarà l¡hora corresponent
+        }
+
+        float cx, cy, r, ant1, ant2, ant3;
+        void pintar_hora()
+        {
+            DateTime n = DateTime.Now; // hora actual
+            int h = n.Hour;
+            if (h > 12) h -= 12;
+            int s = h * 3600 + n.Minute * 60 + n.Second;
+            lbhora.Text = $"{n.Hour.ToString("d2")}:{n.Minute.ToString("d2")}:{n.Second.ToString("d2")}"; // "d2" -> {"1" -> "01", "13" -> "13"}
+
+            Graphics e = this.CreateGraphics();
+
+            float ah = simplificar(90 - s / 120), am = simplificar(90 - s / 10), aseg = simplificar(90 - n.Second * 6); // angles corresponents
+
+            // Despintar línees antigues (pintar-les del color de fons
+            e.DrawLine(new Pen(Color.SeaGreen, 8), cx, cy, cx + (r * 0.9f) * cos(ant2), cy + (r * 0.9f) * sin(ant2)); // minuts
+            e.DrawLine(new Pen(Color.SeaGreen, 15), cx, cy, cx + (r * 0.4f) * cos(ant1), cy + (r * 0.4f) * sin(ant1)); // hores
+            e.DrawLine(new Pen(Color.SeaGreen, 1), cx, cy, cx + (r * 0.95f) * cos(ant3), cy + (r * 0.95f) * sin(ant3)); // segons
+
+            // Dibuixar segments de referècia
+            for (int i = 0; i < 60; i++)
+            {
+                float an = i * 6;
+                int thick = 2, llarg = 20;
+                if (i % 5 == 0)
+                {
+                    thick = 8;
+                    llarg = 35;
+                }
+                e.DrawLine(new Pen(fff, thick), cx + (r - 5) * cos(an), cy + (r - 5) * sin(an), cx + (r - llarg) * cos(an), cy + (r - llarg) * sin(an));
+            }
+
+            // Pintar noves (blanc
+            e.DrawLine(new Pen(fff, 8), cx, cy, cx + (r * 0.9f) * cos(am), cy + (r * 0.9f) * sin(am)); // minuts
+            e.DrawLine(new Pen(fff, 15), cx, cy, cx + (r * 0.4f) * cos(ah), cy + (r * 0.4f) * sin(ah)); // hores
+            e.DrawLine(new Pen(Color.DarkRed, 1), cx, cy, cx + (r * 0.95f) * cos(aseg), cy + (r * 0.95f) * sin(aseg)); // segons
+            
+            // Ara els angles antics seran els que hem usat
+            ant1 = ah;
+            ant2 = am;
+            ant3 = aseg;
+        }
+
+        private void paint(object sender, PaintEventArgs e)
+        {
+            // Només cercle principal (inici del programa i canvi de tamany, ja que el cercle és constant)
+            cx = Width / 2;
+            cy = (Height + lbhora.Height - 40) / 2;
+            r = cy - lbhora.Height - 25;
+
+            pintar_hora();
+
+            e.Graphics.DrawEllipse(new Pen(fff, 5), cx - r, cy - r, 2 * r, 2 * r);
+            e.Graphics.FillEllipse(Brushes.SeaGreen, cx - r, cy - r, 2 * r, 2 * r);
+        }
+    }
+}
