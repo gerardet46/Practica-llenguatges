544553497bc685245ebc110bc9f4214061039aef
GerrySoft <33908768+GerrySoft@users.noreply.github.com>
2018-06-07 13:17:09 +0200

    Laberint: té sortida?
    Aquest algoritme es basa en començar del punt de partida, i comprovar en quines direccions ens podem moure. Si hem pogut arribar a la casella actual, igual podrem avançar, però sinó és impossible. Quan anam a l'esquerra o al nord, hem de tornar enrere per a comprovar les caselles noves.
    En resum, es pintarà tot el que no sigui tancat des del punt de partida de color fúcsia.

--- Javascript/Laberint (amb p5) - Tenir sortida o no/sketch.js
+++ Javascript/Laberint (amb p5) - Tenir sortida o no/sketch.js
@@ -2,21 +2,22 @@ let b = "", c = "", max = 30, w = 600 / max, current = null;
 
 function setup() {
 	createCanvas(601,601);
-	background(230);
+	background(230); // Posam com a color de fons un gris clar (#)
 
-	for (let x = 0; x <= max; x++) {
+	for (let x = 0; x <= max; x++) { // Dibuixam les línees de separació
 		line(0, x * w, 600, x * w);
 		line(x * w, 0, x * w, 600);
 	}
-	for (let i = 0; i < max * max; i++) b += '.';
+	for (let i = 0; i < max * max; i++) b += '.'; // '.' és un espai en blac i 'P' és una paret
 	c = b;
 }
 let borrar = false, corre = false, ind = -1;
 function draw() {
 	if (borrar) {
+		// Borram
 		fill(230);
 		for (let i = 0; i < b.length; i++) {
-			if (b[i] == 'W') {			
+			if (b[i] == 'p') {			
 				let p = _p(i);
 				rect(p[0] * w, p[1] * w, w, w);
 			}
@@ -25,26 +26,26 @@ function draw() {
 		for (let i = 0; i < max * max; i++) b += '.';
 		c = b;
 		borrar = false;
-		if (corre) window.location.href = window.location.href;
+		if (corre) window.location.href = window.location.href; // Actualitzar la pàgina
 	}
 	else if (corre) {
-		if (b[0] == 'W' || b[b.length - 1] == 'W') {
+		if (b[0] == 'p' || b[b.length - 1] == 'p') { // Si comença o acaba amb un mur, no té sortida
 			corre = false;
 			alert("No té sortida");
-		} else if (++ind <= max * max) {
+		} else if (++ind <= max * max) { // ja que draw() s'executa infinitament, usar ++ind és com fer un bucle del tipus for, però així podrem veure com evoluciona l'algoritme
 			if (ind % max == 0) console.log(ind);
-			if (ind == max * max) {
+			if (ind == max * max) { // Hem acabat
 				if (b[b.length - 1] == 't') alert("Sortida trobada, reiniciant en 5s");
 				else alert("No té sortida, reiniciant en 5s");
 				sleep(5000);
-				window.location.href = window.location.href
+				window.location.href = window.location.href;
 				return;
-			} if (ind == 0) {
-				fill(200,20,80);
+			} if (!ind) { // Si l'index és 0, posam la primera casella seleccionada (punt de partida)
+				fill(200,20,80); // Fúcsia (més vermell que blau, i aquest més que verd)
 				b = b.r(0, 't');
 				rect(0,0,w,w);
 			}
-			if (b[ind] == 't') {
+			if (b[ind] == 't') { // Si podem continuar a partir de la casella, n'afegirem més segons si està buida
 				if (b[b.length - 1] == 't') {
 					alert("Sortida trobada, reiniciant en 5s");
 					sleep(5000);
@@ -54,51 +55,53 @@ function draw() {
 				fill(200,20,80);
 				
 				let p = _p(ind), po = null, lf = false, top = false;
-				if (p[0] + 1 < max && b[ind + 1] == '.') {
+				if (p[0] + 1 < max && b[ind + 1] == '.') { // veïna de la dreta
 					b = b.r(ind + 1, 't');
 					po = _p(ind + 1);
 					rect(po[0] * w, po[1] * w, w, w);
 				}
-				if (ind + max < max * max && b[ind + max] == '.') {
+				if (ind + max < max * max && b[ind + max] == '.') { // veïna del sud
 					b = b.r(ind + max, 't');
 					po = _p(ind + max);
 					rect(po[0] * w, po[1] * w, w, w);
 				}
-				if (p[0] && b[ind - 1] == '.') {
+				if (p[0] && b[ind - 1] == '.') { // A l'esquerra
 					b = b.r(ind - 1, 't');
 					po = _p(ind - 1);
 					rect(po[0] * w, po[1] * w, w, w);
 					lf = true;
 				}
-				if (ind - max >= 0 && b[ind - max] == '.') {
+				if (ind - max >= 0 && b[ind - max] == '.') { // Al nord
 					b = b.r(ind - max, 't');
 					po = _p(ind - max);
 					rect(po[0] * w, po[1] * w, w, w);
 					top = true;
 				}
+				// Retrocedim si hem pogut anar a l'esquerra o adalt
 				if ((top && lf) || top) ind -= (max + 1);
 				else if (lf) ind -= 2;
 			}
 		}
 	}
 	else if (mouseX < 601 && mouseY < 601 && mouseX >= 0 && mouseY >= 0) {
-		let a = _i(~~(mouseX / w), ~~(mouseY / w));
+		let a = _i(~~(mouseX / w), ~~(mouseY / w)); // Obtenim la cel·la en la què estam
 
 		if (!mouseIsPressed) current = null;
+		// Pintam o despintam la cel·la actual (current)
 		if (current != a && mouseIsPressed) {
-			if (b[a] == '.') b = b.r(a, 'W');
+			if (b[a] == '.') b = b.r(a, 'p');
 			else b = b.r(a, '.');
 
 			current = a;
 		}
 		for (let i = 0; i < b.length; i++) {
-			if (b[i] == 'W' && c[i] == '.') {
+			if (b[i] == 'p' && c[i] == '.') {
 				fill(80);
 				let p = _p(i);
 				rect(p[0] * w, p[1] * w, w, w);
 				c = b;
 			}
-			if (b[i] == '.' && c[i] == 'W') {
+			if (b[i] == '.' && c[i] == 'p') {
 				fill(230);
 				let p = _p(i);
 				rect(p[0] * w, p[1] * w, w, w);
@@ -107,8 +110,8 @@ function draw() {
 		}
 	}
 }
-function _i(x, y) {return y * max + x}
-function _p(i) {
+function _i(x, y) {return y * max + x} // Punt a sencer
+function _p(i) { // Nombre enter a punt
 	let y = 0;
 	while (i >= max && ++y) i -= max;
 	return [i, y];
@@ -117,10 +120,10 @@ function _p(i) {
 function borrar_click() {borrar = true}
 function comprovar_click() {corre = true}
 
-function sleep(milliseconds) {
+function sleep(milliseconds) { // Pausar l'execució durant n mil·lèsimes
   var start = new Date().getTime();
   while (1)
     if ((new Date().getTime() - start) > milliseconds) return;
 }
 
-String.prototype.r = function(i, r) {return this.substr(0, i) + r + this.substr(i + 1)}
+String.prototype.r = function(i, ch) {return this.substr(0, i) + ch + this.substr(i + 1)} // funció per reemplaçar un caràcter ch a l'index i
